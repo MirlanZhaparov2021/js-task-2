b5958e3a8614b19cfd4ef8002cdb477a533b4d99
Mirlan Zhaparov <mirlan.zhaparov.on@gmail.com>
2022-01-13 21:46:23 +0300

    add task1

---
+++ js/task1.js
@@ -0,0 +1,9 @@
+//let str = '"its great" he seid';
+//console.log(str);
+//let firstName = "Chak";
+//let secondName = "Norris";
+//let fullNme = firstName.concat(" ", secondName);
+//console.log(fullNme);
+
+let sentence = "ddd@bbb@ccc";
+console.log(sentence.replace("@", "!"));
